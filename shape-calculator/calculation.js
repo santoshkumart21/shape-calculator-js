a3bb1a6f13d3996593146efb0d65d6ac49bdd548
santoshkumart21 <90909427+santoshkumart21@users.noreply.github.com>
2022-05-24 12:18:37 +0530

    Update calculation.js

--- shape-calculator/calculation.js
+++ shape-calculator/calculation.js
@@ -3,14 +3,14 @@ export function shapeResult(number1, number2) {
   this.number2 = number2;
 
   this.squareRes = () => {
-    return this.number1 * this.number1;
+    return this.number1 ** this.number1;
   };
 
   this.reactangleRes = () => {
     return this.number1 * this.number2;
   };
   this.circleRes = () => {
-    return this.number1 ** 2;
+    return this.number1 * 2;
   };
   this.ellipseRes = () => {
     return Math.PI * (this.number1 * this.number2);
