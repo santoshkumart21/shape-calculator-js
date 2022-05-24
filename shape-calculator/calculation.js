a2102ee4da5ceb6afa23d8401eae351ba8aaffa8
santoshkumart21 <90909427+santoshkumart21@users.noreply.github.com>
2022-05-24 12:18:01 +0530

    Update calculation.js

--- shape-calculator/calculation.js
+++ shape-calculator/calculation.js
@@ -10,7 +10,7 @@ export function shapeResult(number1, number2) {
     return this.number1 * this.number2;
   };
   this.circleRes = () => {
-    return this.number1 * 2;
+    return this.number1 ** 2;
   };
   this.ellipseRes = () => {
     return Math.PI * (this.number1 * this.number2);
